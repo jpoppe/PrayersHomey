3c438901003c26a9a0a5e2b77080fce6d51f2d60
omar <dpanet@hotmail.com>
2019-02-08 09:57:44 +0400

    commit

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv = require('dotenv');
 dotenv.config();
 import *  as prayer from '@dpanet/prayers-lib';
 
+
 let prayers: Array<object> =
     [{ prayerName: 'Fajr', prayerTime: "2019-01-31T05:46:00.000Z" },
     { prayerName: 'Sunrise', prayerTime: "2019-01-31T07:02:00.000Z" },
